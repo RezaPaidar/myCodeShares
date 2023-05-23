0ba0e395ad0bd5d6a74dde970844e876e8ea259d
Reza paidar <reza.paidar@outlook.com>
2023-05-23 19:22:58 +0330

    Update ConvertList2Datatable.cs

--- ConvertList2Datatable.cs
+++ ConvertList2Datatable.cs
@@ -1,1 +1,24 @@
-
+//Convert List to Datatable
+public static DataTable DTDataSource<T>(this IList<T> data)
+{
+	DataTable dt = new DataTable();
+	PropertyDescriptorCollection props = TypeDescriptor.GetProperties(typeof(T));
+	for (int i = 0; i < props.Count; i++)
+	{
+		PropertyDescriptor prop = props[i];
+		if (prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+			dt.Columns.Add(prop.Name, prop.PropertyType.GetGenericArguments()[0]);
+		else
+			dt.Columns.Add(prop.Name, prop.PropertyType);
+	}
+	object[] values = new object[props.Count];
+	foreach (T item in data)
+	{
+		for (int i = 0; i < values.Length; i++)
+		{
+			values[i] = props[i].GetValue(item);
+		}
+		dt.Rows.Add(values);
+	}
+	return dt;
+}
